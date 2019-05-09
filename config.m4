2e971ab58b2c12dec94d0f80fdcf489940b01048
Sara Golemon <pollita@php.net>
2019-05-09 14:25:20 -0400

    Link against libgmp

--- config.m4
+++ config.m4
@@ -1,6 +1,24 @@
 dnl config.m4
-PHP_ARG_ENABLE(mygmp)
+PHP_ARG_WITH(mygmp, for GNU MP support,
+[  --with-mygmp[=DIR]          Include GNU MP support])
 
 if test "$PHP_MYGMP" != "no"; then
+  dnl Look for libgmp in explicit, or common implicit places.
+  for i in $PHP_MYGMP /usr/local /usr; do
+    if test -f $i/include/gmp.h; then
+      MYGMP_DIR=$i
+      break
+    fi
+  done
+
+  if test -z "$MYGMP_DIR"; then
+    AC_MSG_ERROR(Unable to locate gmp.h)
+  fi
+
+  dnl Update library list and include paths for libgmp
+  PHP_ADD_LIBRARY_WITH_PATH(gmp, $MYGMP_DIR/$PHP_LIBDIR, MYGMP_SHARED_LIBADD)
+  PHP_ADD_INCLUDE($MYGMP_DIR/include)
+  PHP_SUBST(MYGMP_SHARED_LIBADD)
+
   PHP_NEW_EXTENSION(mygmp, mygmp.c, $ext_shared)
 fi
--- mygmp.c
+++ mygmp.c
@@ -3,16 +3,48 @@
 #endif
 
 #include "php.h"
+#include "ext/standard/info.h"
+#include "ext/standard/php_random.h"
+
+#include <gmp.h>
+
+static gmp_randstate_t randstate;
+
+static PHP_MINIT_FUNCTION(mygmp) {
+	zend_ulong seed;
+
+	if (FAILURE == php_random_int_silent(0, ZEND_ULONG_MAX, &seed)) {
+		return FAILURE;
+	}
+
+	gmp_randinit_mt(randstate);
+	gmp_randseed_ui(randstate, seed);
+
+	return SUCCESS;
+}
+
+static PHP_MSHUTDOWN_FUNCTION(mygmp) {
+	gmp_randclear(randstate);
+
+	return SUCCESS;
+}
+
+static PHP_MINFO_FUNCTION(mygmp) {
+    php_info_print_table_start();
+    php_info_print_table_row(2, "MyGMP support", "enabled");
+    php_info_print_table_row(2, "libgmp version", gmp_version);
+    php_info_print_table_end();
+}
 
 zend_module_entry mygmp_module_entry = {
     STANDARD_MODULE_HEADER,
     "mygmp",
     NULL, /* functions */
-    NULL, /* MINIT */
-    NULL, /* MSHUTDOWN */
+    PHP_MINIT(mygmp),
+	PHP_MSHUTDOWN(mygmp),
     NULL, /* RINIT */
     NULL, /* RSHUTDOWN */
-    NULL, /* MINFO */
+    PHP_MINFO(mygmp),
     NO_VERSION_YET,
     STANDARD_MODULE_PROPERTIES
 };
