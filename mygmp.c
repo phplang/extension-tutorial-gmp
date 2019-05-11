2c38b23a9ed2a3cddb7161169b166e7b9cad6701
Sara Golemon <pollita@php.net>
2019-05-11 12:43:27 -0400

    Iterate a list

--- mygmp.c
+++ mygmp.c
@@ -71,6 +71,44 @@ static PHP_FUNCTION(mygmp_add_array) {
 	do_mygmp_add(return_value, Z_STR_P(a), Z_STR_P(b));
 }
 
+static PHP_FUNCTION(mygmp_sum) {
+	zend_array *arr;
+	mpz_t ret;
+	zend_string *retstr;
+	zval *val;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS(), "h", &arr) == FAILURE) {
+		return;
+	}
+
+	mpz_init(ret);
+
+	/* foreach ($arr as $val) */
+	ZEND_HASH_FOREACH_VAL(arr, val) {
+		mpz_t v;
+		zend_string *str = zval_get_string(val);
+
+		mpz_init(v);
+		if (mpz_set_str(v, ZSTR_VAL(str), 0)) {
+			php_error(E_WARNING, "Invalid value encountered: %.*s", (int)ZSTR_LEN(str), ZSTR_VAL(str));
+			mpz_clear(v);
+			zend_string_release(str);
+			continue;
+		}
+
+		mpz_add(ret, ret, v);
+		mpz_clear(v);
+		zend_string_release(str);
+	} ZEND_HASH_FOREACH_END();
+
+	retstr = zend_string_alloc(mpz_sizeinbase(ret, 10), 0);
+	mpz_get_str(ZSTR_VAL(retstr), 10, ret);
+	ZSTR_LEN(retstr) = strlen(ZSTR_VAL(retstr));
+
+	mpz_clear(ret);
+	RETURN_STR(retstr);
+}
+
 static PHP_FUNCTION(mygmp_random_ints) {
 	zend_ulong count;
 	zend_ulong bits = sizeof(unsigned long) << 3;
@@ -101,6 +139,7 @@ static zend_function_entry mygmp_functions[] = {
 	PHP_FE(mygmp_get_version, NULL)
 	PHP_FE(mygmp_add, NULL)
 	PHP_FE(mygmp_add_array, NULL)
+	PHP_FE(mygmp_sum, NULL)
 	PHP_FE(mygmp_random_ints, NULL)
 	PHP_FE_END
 };
